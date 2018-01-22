232ff23510817260fb9dc1bbac296ecd0b08464e
ellefeg <ellefeg@users.noreply.github.com>
2018-01-22 14:43:44 +0100

    Update PairwiseExonerate.sh
    Removed comment because I think the script does this manually now
    # Optional: Run /home/laura/scripts/Blast2SplitHits.sh to retrieve sequences without BLAST hits (required for TransDecoder downstream)
    
    Made some of the comments clearer
    
    Removed the mail command
    
    Split up the section "Split Output, Remove Duplicates, Select Fixed Sequences" to make it clearer
    
    I was having problems with duplicates, but I think i fixed it by changing this line:
    # was
    cut -f 1 "$sample"_exonerateCigarFS.out | sort -k1,1 > "$sample"_FScorrectedgenes.list
    # now
    cut -f 1 "$sample"_exonerateCigarFS.out | sort -k1,1 | uniq > "$sample"_FScorrectedgenes.list
    
    # Added cd-hit-est step which removes 100% redundant sequences at the end because they weren't getting filtered out properly before

--- dependencies/PairwiseExonerate.sh
+++ dependencies/PairwiseExonerate.sh
@@ -17,6 +17,9 @@
 ## Altered output to produce fasta file containing ONLY edited sequences (full fasta files will be in temp folder)
 # EDIT 25.07.17
 ## Altered output to produce fasta file containing edited sequences + the rest of the transcriptome
+# EDIT 22.01.18
+## Revised comments, slight changes to filtering identical sequences
+## Included CD-HIT-EST command to remove redundant sequences
 # ------------------------------------------------------------------
 
 ##########################
@@ -24,54 +27,47 @@
 ##########################
 
 # Run BLASTx with nt.fa and database of interest (--outfmt 6)
-# Optional: Run /home/laura/scripts/Blast2SplitHits.sh to retrieve sequences without BLAST hits (required for TransDecoder downstream)
-# You will need: (1) short name for sample (2) blast output (3) nt.fa (4) aa.fa underlying database used for BLASTx
+# You need to know the following information: (1) short name for sample (2) blast output [of #3 vs db of #4] (3) nt.fa (4) aa.fa underlying BLASTx db
 
 #################################
 ###  Prepare pairwise table   ###
 #################################
 
-# Creates a table with headers/sequences of each blast pair
-## NB: Allows for duplicates in the protein list (i.e. the same protein appearing multiple times)
+# Creates a table matching header+sequence of query with header+sequence of hit/subject. In the next step this table will be read line-by-line so Exonerate knows which two sequences to match up
+## Approach: use the "query" column of the blast hit table to pull out the nucleotide sequence information, and then use the "subject" column to pull out the amino acid sequence information
 ## NB: Columns will be {aa name} {aa seq} {nt name} {nt seq}
-
-echo "Preparing pairwise table at $(date)"
+## NB: Allows for duplicates in the subject protein list (i.e. the same aa sequence can be the best hit for multiple nt queries)
+echo "Preparing pairwise query-hit table at $(date)"
 
 # Renaming variables
-echo "...renaming variables"
-sample=$1 #sample short name
-blastOut=$2 #blast hits
-nucleo=$3 #nucleotide query fasta
-amino=$4 #blastDb fasta
+sample=$1	#sample short name
+blastOut=$2	#blast hits
+nucleo=$3	#nucleotide query fasta
+amino=$4	#blastDb fasta
 
-# Convert $nucleo and $amino to sorted tables with simple names
-## NB: Start with a command to remove tabs so there shouldn't be any problems in printing the right columns
-## NB: Use awk $NF to select the LAST column. Use instead of sed $3 in case sequences lack headers
-echo "...generating and sorting data tables"
+# Convert $nucleo and $amino fasta files to tabular format and simplify fasta header names
+## NB: The first tab removal step will not change anything in most cases - it is simply to prevent any problems with printing the right columns in case the fasta file is weirdly formatted
+## NB: The awk $NF is used to select the LAST column, and is used instead of sed $3 in case sequences lack headers
 sed 's/\t/ /g' "$nucleo" | fasta_formatter -t | sort -k1,1 | sed 's/ /\t/' | awk '{print $1, $NF}' > "$nucleo".tab
 sed 's/\t/ /g' "$amino" | fasta_formatter -t | sort -k1,1 | sed 's/ /\t/' | awk '{print $1, $NF}' > "$amino".tab
-# Convert $blastOut to sorted table
+# Convert $blastOut to sorted table showing query-hit pairs
 cut -f1-2 "$blastOut" | sort -k1,1 > "$blastOut".tab
 #Assign nicer variable names
-nucleotab="$nucleo".tab
-aminotab="$amino".tab
-blasttab="$blastOut".tab
+nucleotab="$nucleo".tab		#the tabular nt file we just made
+aminotab="$amino".tab		#the tabular aa file we just made
+blasttab="$blastOut".tab	#the tabular blast query-hit file we just made
 
 # Merge $blasttab and $nucleotab
 ## NB: Columns will be {nt name} {aa name} {nt seq}
-echo "...adding nucleotide data to table"
 join $blasttab $nucleotab | sed 's/ /\t/g' | sort -k2,2 > "$blasttab"_nt
-
 # Merge "$blasttab"_nt and $aminotab
 ## NB: Columns will be {aa name} {nt name} {nt seq} {aa seq}
 echo "...adding amino acid data to table"
 join "$blasttab"_nt $aminotab -1 2 -2 1 | sed 's/ /\t/g' > "$blasttab"_nt+aa
-
 # Arrange columns
 ## NB: Columns will be {aa name} {aa seq} {nt name} {nt seq}
 echo "...reorganising sequence table"
 awk '{OFS="\t"; print $1, $4, $2, $3}' "$blasttab"_nt+aa > "$sample"_pairedSeqTab
-
 # Pairwise sequence table is complete
 echo -e "Pairwise sequence table "$sample"_pairedSeqTab complete at $(date).\\n"
 
@@ -80,7 +76,6 @@ echo -e "Pairwise sequence table "$sample"_pairedSeqTab complete at $(date).\\n"
 #############################
 
 # Reads the pairwise table line-by-line, converts entries back to fasta sequences, runs exonerate
-
 echo "Commencing $(wc -l < "$sample"_pairedSeqTab) Exonerate analyses at $(date)"
 
 # Performs each pairwise comparison
@@ -88,28 +83,32 @@ while read aaName aaSeq ntName ntSeq; do
 	echo "^^...analysing "$ntName""
 	echo -e ">"$aaName"\n"$aaSeq > tempAA.fa
 	echo -e ">"$ntName"\n"$ntSeq > tempNT.fa
+	
+	# Now we run exonerate
 	exonerate --model protein2dna --query tempAA.fa --target tempNT.fa --verbose 0 --showalignment 0 --showvulgar no --showcigar yes -n 1 --ryo ">%ti (%tab - %tae)\n%tcs\n" >> "$sample"_exonerateTemp.out
+	
 	echo "^    completed Exonerate for "$ntName" (n sequences = $(grep -c ">" "$sample"_exonerateTemp.out))"
 	rm tempAA.fa tempNT.fa
 done < "$sample"_pairedSeqTab
 
-##################################################################
-###  Split Output, Remove Duplicates, Select Fixed Sequences   ###
-##################################################################
 
-# Added/modified 20 July 2017, 21 July 2017
+#############################################################
+###  Split Cigar and Sequence Output, Filter Redundancy   ###
+#############################################################
 
-# The output currently contains Cigar alignments plus fasta sequences - split up, and remove duplicates
+# Added/modified 20 July 2017, 21 July 2017, 22 Jan 2018
+# At this stage, "$sample"_exonerateTemp.out contains Cigar alignments and fasta sequences of FS corrected sequences - split up into separate Cigar and Fasta files
 
-# Extract Cigar alignments
+# Extract all Cigar alignments
 echo -e "\nExtracting Cigar output"
-grep "cigar:" "$sample"_exonerateTemp.out | sort | uniq > "$sample"_exonerateCigar.out
+grep "cigar:" "$sample"_exonerateTemp.out | sort | uniq > "$sample"_exonerateCigar.out #extract all unique Cigar rows
 echo "...extracted $(grep -c "cigar:" "$sample"_exonerateCigar.out) alignments. NB: $(($(grep -c "cigar:" "$sample"_exonerateTemp.out)-$(grep -c "cigar:" "$sample"_exonerateCigar.out))) duplicates removed."
-#Extract only Cigar alignments with frameshift correction
+# Extract only Cigar alignments with frameshift correction
+## NB: A frameshift correction has occurred if the Cigar code contains "D" or "I" (i.e. a deletion/insertion relative to the reference)
 grep "cigar:" "$sample"_exonerateCigar.out | cut -f 6,11- -d " " | sed 's/ /\t/' | awk -F"\t" '$2 ~ /D|I/' > "$sample"_exonerateCigarFS.out
 echo -e "There are $(wc -l < "$sample"_exonerateCigarFS.out) frameshift-corrected sequences with an average of $(cut -f 2 "$sample"_exonerateCigarFS.out | sed 's/ //g;s/[0-9]//g' | sed 's/DI/x/g;s/ID/y/g' | sed 's/M//g' | awk '{ print length }' | awk '{sum+=$1} END {print sum / NR}') frameshifts each.\n"
 # Get list of changed genes
-cut -f 1 "$sample"_exonerateCigarFS.out | sort -k1,1 > "$sample"_FScorrectedgenes.list
+cut -f 1 "$sample"_exonerateCigarFS.out | sort -k1,1 | uniq > "$sample"_FScorrectedgenes.list
 
 # Extract fasta sequences 
 echo "Extracting fasta output"
@@ -119,10 +118,16 @@ grep -v "cigar:" "$sample"_exonerateTemp.out | fasta_formatter -w 0 | fasta_form
 join "$sample"_FScorrectedgenes.list "$sample"_exonerateFastaAll.tab | sed 's/ /\t/g' | sed 's/^/>/g' | sed 's/\t/\n/g' > "$sample"_exonerateFasta_FScorrected.fa
 echo -e "...extracted $(grep -c ">" "$sample"_exonerateFasta_FScorrected.fa) edited sequences. NB: $(($(grep -c ">" "$sample"_exonerateTemp.out)-$(grep -c ">" "$sample"_exonerateFastaAllTested.out))) duplicates removed.\n"
 
+##############################################
+###  Merge Changed + Unchanged Sequences   ###
+##############################################
+
 # Make a full dataset (fixed + unfixed)
 fastaremove -f $nucleo -r "$sample"_FScorrectedgenes.list > uncorrectedgenes.fa
-cat uncorrectedgenes.fa >> "$sample"_TrinityFS.fa
-cat "$sample"_exonerateFasta_FScorrected.fa >> "$sample"_TrinityFS.fa
+cat uncorrectedgenes.fa >> "$sample"_TrinityFS_redundant.fa
+cat "$sample"_exonerateFasta_FScorrected.fa >> "$sample"_TrinityFS_redundant.fa
+# Remove sequence redundancy (100% ID)
+cd-hit-est -i "$sample"_TrinityFS_redundant.fa -o "$sample"_TrinityFS.fa -c 1
 
 # Exonerate analysis is complete
 echo "Exonerate runs complete at $(date)."
@@ -143,11 +148,9 @@ mkdir "$sample"_tempfiles
 mv "$sample"_exonerateFasta_FScorrected.fa "$sample"_FScorrected.fa
 mv "$sample"_exonerateCigarFS.out "$sample"_FScorrected.cigar
 
-for i in "$nucleo".tab "$amino".tab "$blastOut".tab "$blasttab"_nt "$blasttab"_nt+aa "$sample"_pairedSeqTab "$sample"_exonerateTemp.out "$sample"_exonerateCigar.out "$sample"_FScorrectedgenes.list "$sample"_exonerateFastaAll.tab "$sample"_exonerateFastaAllTested.out uncorrectedgenes.fa
+for i in "$nucleo".tab "$amino".tab "$blastOut".tab "$blasttab"_nt "$blasttab"_nt+aa "$sample"_pairedSeqTab "$sample"_exonerateTemp.out "$sample"_exonerateCigar.out "$sample"_FScorrectedgenes.list "$sample"_exonerateFastaAll.tab "$sample"_exonerateFastaAllTested.out uncorrectedgenes.fa "$sample"_TrinityFS_redundant.fa
 	do
 	mv $i "$sample"_tempfiles
 done
 
 echo -e "Frameshift correction analysis complete at $(date).\nWhere are the output files?\n\t> Working directory: $(pwd)\n\t> Final whole-transcriptome file: "$sample"_TrinityFS.fa \n\t> Final corrected sequences only: "$sample"_FScorrected.fa\n\t> Corrected cigar output: "$sample"_FScorrected.cigar\n\t> Temporary file directory: "$sample"_tempfiles"
-echo "Exonerate complete for all samples at $(date)" | mail -s "Server alert: Exonerate complete" "lfgrice@gmail.com"
-
