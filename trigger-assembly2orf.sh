09e99c3d3de02f99b7dccef9343fa747915268a1
ellefeg <ellefeg@users.noreply.github.com>
2018-09-25 09:34:54 +0200

    user email alert, no scriptlib requirement
    * Removed need for user to set dependencies location every time, they can change it if need be
    WAS:
    scriptlib=$3	#location of the script library containing the dependencies
    NOW:
    scriptlib=/home/laura/scripts/assembly2orf/dependencies
    
    * the script now sends an email to a custom address at the end of the run

--- trigger-assembly2orf.sh
+++ trigger-assembly2orf.sh
@@ -6,7 +6,7 @@ set -e
 # Version:	v01.01
 # Goal:		To feed the samples in to assembly2orf.sh 
 # Requirements:	The user must have already made a tab-delimited file ("input_param") listing the sample ID and file location of each transcriptome of interest
-# Usage:	nohup ./TriggerPipeline.sh {paramfile} {wkdir} {dependencies} {blastdb} {blastfasta} > TransPipeline_nohup.out 2>&1&
+# Usage:	nohup ./TriggerPipeline.sh {paramfile} {wkdir} {blastdb} {blastfasta} {email} > TransPipeline_nohup.out 2>&1&
 # ------------------------------------------------------------------
 # VERSION INFORMATION
 # v01.00 28 July 2017
@@ -30,9 +30,12 @@ fi
 # Rename variables
 paramfile=$1 	#location of "input_param" file, full file string
 wkdir=$2 	#desired working directory location
-scriptlib=$3	#location of the script library containing the dependencies
-blastdb=$4	#location of the blast database to use
-blastfasta=$5	#location of the fasta file used to create this blastdb
+blastdb=$3	#location of the blast database to use
+blastfasta=$4	#location of the fasta file used to create this blastdb
+email=$5	#user email to recieve email at the end of the run
+
+# set location of dependencies - user can change this if required
+scriptlib=/home/laura/scripts/assembly2orf/dependencies
 
 # ------------------------------------------------------------------
 # Run assembly2orf pipeline on all samples of interest
@@ -41,3 +44,9 @@ while read -r sample_name transcripts; do
 	cd "$workdir" || exit 1
 	"$scriptlib"/assembly2orf.sh "$sample_name" "$transcripts" "$wkdir" "$scriptlib" "$blastdb" "$blastfasta"
 done < "$paramfile"
+
+# ------------------------------------------------------------------
+# Send "Program Complete" email
+# ------------------------------------------------------------------
+
+echo -e "assembly2orf complete for $(grep -c "[A-Za-z]" "$paramfile") samples at $(date).\nYour output can be found in directory "$wkdir"" | mail -s "Asellus server alert: assembly2orf complete" "$email"
